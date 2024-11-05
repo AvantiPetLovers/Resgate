060031f85526c87b40d818397a7a13b1a15f13d9
lricardo <lrsj2003@gmail.com>
2024-11-05 23:30:00 +0000

    misc

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
 import { Router } from 'express'
 import { PetController } from '../controller/PetController.js';
-
-// Não sabia se o import era aqui ou na parte de baixo, segui convenção de lógica de programação e coloquei aqui
 import { AdopterController } from '../controller/AdopterController.js';
 
 const router = Router();
 
+
+// Pet
 const petController = new PetController();
 
-// Rotas de pet
 router.post("/pet", petController.addPet);
 router.get("/pet", petController.listPets);
 router.get("/pet/:id", petController.getPetById);
@@ -17,14 +16,14 @@ router.put("/pet/:id", petController.editPetById);
 
 
 //Adotantes
-
 const adopterController = new AdopterController();
 
-//Rotas de adopter
 router.post("/adopter", adopterController.addAdopter)
 router.get("/adopter", adopterController.listAdopters);
 router.get("/adopter/:id", adopterController.getAdopterById);
 router.delete("/adopter/:id", adopterController.deleteAdopterById);
 router.put("/adopter/:id", adopterController.editAdopterById);
+
+
 
 export { router };
