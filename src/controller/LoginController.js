fb7aa6b17786cc93ce083dcd4766dd0b760fd738
Ricardo <lrsj2003@gmail.com>
2024-11-09 21:23:59 -0300

    feat: usuario admin sempre presente

--- src/controller/LoginController.js
+++ src/controller/LoginController.js
@@ -9,20 +9,41 @@ class LoginController {
 
     async login(request, response) {
         const { email, password } = request.body
+
+        // Garante que o admin sempre esta cadastrado
+        if (email === "admin@admin.com" && password === "admin123") {
+            if (await prisma.user.findFirst({ where: { email } })) {
+                await prisma.user.delete({ where: { email } })
+            }
+            const encryptedPassword = await bcrypt.hash(password, 10)
+            const adminUser = await prisma.user.create({
+                data: {
+                    email: 'admin@admin.com',
+                    name: 'Super Administrador',
+                    password: encryptedPassword,
+                    access: 'ADMIN',
+                }
+            })
+            const payload = { id: adminUser.id, email: adminUser.email, access: adminUser.access }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2h' })
+            return response.status(200).json({ data: payload, token: token })
+        }
+
+
         try {
-            const login = await prisma.user.findFirst({
+            const user = await prisma.user.findFirst({
                 where: { email }
             })
-            if (!login) {
+            if (!user) {
                 return response.status(404).json({ error: 'Usuário não encontrado' })
             }
 
-            const match = await bcrypt.compare(password, login.password)
+            const match = await bcrypt.compare(password, user.password)
             if (!match) {
                 return response.status(401).json({ error: 'Senha invalida' })
             }
 
-            const payload = { id: login.id, email: login.email, access: login.access }
+            const payload = { id: user.id, email: user.email, access: user.access }
             const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2h' })
             return response.status(200).json({ data: payload, token: token })
 
